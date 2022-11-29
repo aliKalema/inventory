ebeda438104781a5a6a4dc2df4bcd2118fab9659
Ali Kalema <aliKalema98@gmail.com>
2022-11-29 06:56:13 +0300

    refacored

--- src/main/java/co/ke/personal/inventory/repository/ItemRepository.java
+++ src/main/java/co/ke/personal/inventory/repository/ItemRepository.java
@@ -17,7 +17,7 @@ public interface ItemRepository extends CrudRepository<Item,Long> {
     @Query(value = "SELECT * FROM item t WHERE cast(created as date) = :localDate",nativeQuery = true)
     List<Item> findByCreated(LocalDate localDate);
 
-    @Query(value="SELECT * FROM  item t LEFT JOIN inventory_movement im ON t.ref_id = im.item_ref_id AND cast(im.moved_date as date) = ? AND im.move_type = ?", nativeQuery = true)
+    @Query(value="SELECT t.name, im.quantity FROM  item t LEFT JOIN  inventory_movement im ON t.ref_id = im.item_ref_id AND cast(im.moved_date as date) = ? AND im.move_type = ?", nativeQuery = true)
     public List<Item> findByMovedDateAndMoveType(LocalDate localDate, String moveType);
 
     @Query(value="SELECT * FROM  item t LEFT JOIN inventory_movement im ON t.ref_id = im.item_ref_id AND cast(im.moved_date as date) >= :startDate AND cast(im.moved_date as date) =< :endDate AND im.move_type = :moveType", nativeQuery = true)
